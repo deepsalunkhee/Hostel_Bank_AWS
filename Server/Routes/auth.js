d9cbafa4ba98a7e0f13ee0ec22e9ee5872894f86
deepsalunkhee <deepsalunkhee@gmail.com>
2024-02-01 22:45:34 +0530

    Comnpleted the UI and route to disply the joined groups

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -5,7 +5,7 @@ dotenv.config();
 // Middleware function to verify user token
 const verifyUser = (req, res, next) => {
     const token = req.header("token");
-    // console.log(token);
+    //  console.log(token);
     if (!token) {
         return res.status(401).json({ error: "Please login" });
     }
